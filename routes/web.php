759bcb062207b3b0ab91123e75f501b65966526e
Ortenc Koci <ortenc.koci@gmail.com>
2023-04-26 17:04:31 +0200

    dasd

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('post' , function () {
+    return view ('post');
+});
+
+Route::get('posts' , function () {
+    return view ('posts');
+});
